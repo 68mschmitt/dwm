5ad799d8df11bdf1a02219984a10fc83e601b65d
Mike Schmitt <68mschmitt@gmail.com>
2025-07-11 20:24:57 -0400

    Hotkey for random wallpaper script

--- config.def.h
+++ config.def.h
@@ -63,9 +63,10 @@ static const char *browser[] = { "firefox", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_b,      spawn,          {.v = browser } },
+	{ MODKEY|ShiftMask,             XK_w,      spawn,          SHCMD("~/.scripts/random-wallpaper.sh") },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      spawn,          {.v = browser } },
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
